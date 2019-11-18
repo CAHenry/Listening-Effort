b33cf22244b04cfafff85954212ad6623190dc05
Craig <craigahenry50@gmail.com>
2019-11-18 13:43:11 +0000

    add denmark_demo

---
+++ Unity/Denmark_demo/Assets/MainScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+
+public class MainScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadDevice());
+    }
+
+    IEnumerator LoadDevice()
+    {
+        XRSettings.LoadDeviceByName("cardboard");
+        yield return null;
+        XRSettings.enabled = true;
+    }
+   
+}
