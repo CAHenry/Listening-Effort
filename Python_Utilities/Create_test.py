8aefdbb78dfa7ba0709a4c1a5eb80c125fa7f4ef
craig <craigahenry50@gmail.com>
2019-11-22 17:04:31 +0000

    Add new python script and tools for creating XMLs and text files for different test conditions and masking conditions

---
+++ Python_Utilities/Create_test.py
@@ -0,0 +1,21 @@
+import Test_classes as le
+
+babble_front = le.MaskingCondition("Babble_front", 1)
+babble_front.set_source_positions([1, 0])
+babble_front.set_audio_files("Babble_1.wav")
+
+babble_behind = le.MaskingCondition("Babble_behind", 1)
+babble_behind.set_source_positions([1, 180])
+babble_behind.set_audio_files("Babble_1.wav")
+
+around = le.MaskingCondition("around", 4)
+around.set_source_positions([[1, 0], [1, 90], [1, 180], [1, 270]])
+around.set_audio_files(["Babble_1.wav", "Babble_2.wav", "Babble_3.wav", "Babble_4.wav"])
+
+babble_front_2_TC = le.TestCondition(babble_front, 2)
+babble_behind_1_TC = le.TestCondition(babble_behind, 1)
+around_3_TC = le.TestCondition(around, 3)
+
+
+test_one = le.Test([babble_behind_1_TC, around_3_TC, babble_front_2_TC])
+test_one.output("D:\\Documents\\Docs\\Imperial\\Listening-Effort\\XMLs", "Babble_front_2", "D:\\Documents\\Docs\\Imperial\\Listening-Effort\\Max\\ASLQ")
---
+++ Python_Utilities/Test_classes.py
@@ -0,0 +1,154 @@
+import numpy as np
+import os
+
+class Test:
+    _test_conditions = []
+
+    def __init__(self, test_conditions):
+        self._test_conditions.extend(test_conditions)
+
+    def add_condition(self, test_condition):
+        self._test_conditions.extend(test_condition)
+
+    def output(self, root, test_name, sentences):
+
+        def write_source(file, num, pos, name, location, vol, vol_db, slider_pos=45):
+            file.write("\t<Source%d_x>%.9f</Source%d_x>\n" % (num, pos[0], num))
+            file.write("\t<Source%d_y>%.9f</Source%d_y>\n" % (num, pos[1], num))
+            file.write("\t<Source%d_z>%.9f</Source%d_z>\n" % (num, 0, num))
+            file.write("\t<Source%d_vol>%.9f</Source%d_vol>\n" % (num, vol, num))
+            file.write("\t<Source%d_vol_dB>%.9f</Source%d_vol_dB>\n" % (num, vol_db, num))
+            file.write("\t<Source%d_sliderPosition>%d</Source%d_sliderPosition>\n" % (num, slider_pos, num))
+            file_path = location + "/" + name
+            file.write("\t<Source_%d_filePath>%s</Source_%d_filePath>\n" % (num, file_path, num))
+        xml_filename = test_name + ".xml"
+        text_filename = test_name + ".txt"
+
+        xml = open(os.path.join(root, xml_filename), "w+")
+        text = open(os.path.join(root, text_filename), "w+")
+
+        maskers = []
+        files = []
+        positions = []
+        source_count = 0
+        for condition in self._test_conditions:
+            masker = condition.masking_condition
+            num_sources = masker.get_num_sources()
+
+            if masker.name not in maskers:
+                maskers.append(masker.name)
+                files.extend(masker.get_audio_files())
+                positions.extend(masker.get_source_positions())
+                masker.source_index = source_count
+                source_count += num_sources
+
+            text.write("%s, %i, %i , %i\n" % (masker.name, masker.source_index, num_sources, condition.screen))
+        text.close()
+
+        source_count += len(os.listdir(sentences))
+        xml.write("<BinauralApp>\n"
+                  "\t<FrameSize>%d</FrameSize>\n"
+                  "\t<ListenerPosX>0.000000000</ListenerPosX>\n"
+                  "\t<ListenerPosY>0.000000000</ListenerPosY>\n"
+                  "\t<ListenerPosZ>0.000000000</ListenerPosZ>\n"
+                  "\t<ListenerOrX>-0.000000000</ListenerOrX>\n"
+                  "\t<ListenerOrY>-0.000000000</ListenerOrY>\n"
+                  "\t<ListenerOrZ>0.000000000</ListenerOrZ>\n"
+                  "\t<ListenerOrW>1.000000000</ListenerOrW>\n"
+                  "\t<Platform>Windows</Platform>\n"
+                  "\t<OSCListenPort>12300</OSCListenPort>\n"
+                  "\t<ReverbOrder>%s</ReverbOrder>\n"
+                  "\t<NumSources>%d</NumSources>\n" % (256, "3D", source_count))
+        source = 0
+
+        gain = 0.501187
+        for i, filename in enumerate(files):
+            # TODO: make reduction of gain here correct for number of sources - think about distance?
+            asdf = positions[i]
+            x, y = positions[i]
+
+            cart = spherical_2_cartesian(1, x, y)
+            write_source(xml, source, cart, filename, "./../Max/Masking", gain * 0.501187,
+                         20 * np.log10(gain * 0.501187))
+            source += 1
+            xml.write("\n")
+
+        for filename in os.listdir(sentences):
+            cart = spherical_2_cartesian(1, 0, 0)
+            write_source(xml, source, cart, filename, "./../Max/ASLQ", gain, 20 * np.log10(gain))
+            source += 1
+            xml.write("\n")
+
+        xml.write("</BinauralApp>")
+        xml.close()
+
+
+class TestCondition:
+    masking_condition = 0
+    screen = 0
+
+    def __init__(self, masking_condition, screen):
+        self.masking_condition = masking_condition
+        self.screen = screen
+
+
+class MaskingCondition:
+    _num_sources = 0
+    _sources = 0
+    _files = []
+    source_index = []
+    name = ""
+
+    def __init__(self, name,  num_sources):
+        self.name = name
+        self._num_sources = num_sources
+        self._sources = np.zeros((num_sources, 2))
+        self._files = ["" for _ in range(num_sources)]
+
+
+    def get_num_sources(self):
+        return self._num_sources
+
+    def set_source_positions(self, positions):
+        if self._num_sources == 1:
+            positions = [positions]
+        if len(positions) != self._num_sources:
+            print("Number of positions does not match the number of sources")
+            return
+
+        for i, position in enumerate(positions):
+            self._sources[i] = position
+
+    def get_source_positions(self):
+        return self._sources
+
+    def set_audio_files(self, files):
+        if type(files) is str:
+            files = [files]
+        for i in range(self._num_sources):
+            if len(files) == 1:
+                self._files[i] = files[0]
+            else:
+                self._files[i] = files[i]
+
+    def get_audio_files(self):
+        return self._files
+
+    def get_video(self):
+        return self._video
+
+
+# Phi is the angle from the lateral plane, theta is the azimuth and anti-clockwise
+def spherical_2_cartesian(r, phi, theta):
+    theta = np.deg2rad(theta)
+    phi = np.deg2rad(phi)
+
+    x = r * np.cos(phi) * np.cos(theta)
+    y = r * np.cos(phi) * np.sin(theta)
+    z = r * np.sin(phi)
+
+    return [x, y, z]
+
+
+
+
